cab1178b1a7c12bba853c6cb0d37f162f3be3936
yi_Xu <xjy123456120@163.com>
2020-08-09 14:44:22 +0800

    更新增加规则的通知的顺序

--- Configuration/ClashForWindows/changeRule.js
+++ Configuration/ClashForWindows/changeRule.js
@@ -1,9 +1,9 @@
 module.exports.parse = (raw, { yaml, notify }) => {
-  notify("profile has been modified.", "Personal rules has been added.", true)
-  const rawObj = yaml.parse(raw)
+  const rawObj = yaml.parse(raw);
   rawObj['rules'] = rawObj['rules'].filter(function (item) {
-    return item.search("v2ex") === -1
-  })
-  rawObj['rules'].unshift("DOMAIN-SUFFIX,v2ex.com,DIRECT")
-  return yaml.stringify(rawObj)
+    return item.search("v2ex") === -1;
+  });
+  rawObj['rules'].unshift("DOMAIN-SUFFIX,v2ex.com,DIRECT");
+  notify("profile has been modified.", "Personal rules has been added.", true);
+  return yaml.stringify(rawObj);
 }
